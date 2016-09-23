b88a8b89b5d1993a0b001c6fa5c35e2ad649e909
leonardo <l.castillo1989@Hotmail.com>
2016-09-22 23:57:52 -0400

    startTicking should be a lil more descriptive or have a tip

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+var digitalClock = {
+	time:  Math.round(Date.now() / 1000),
+    startTicking: function(){
+    	setInterval(function(){this.time += 1;}.bind(this),1000);
+    }
+}
+
+var censor = (word, sentence) => {
+  return sentence.split(word).join("BLEEP");
+}
+
+var violenceCensor = censor.bind(null,"violence");
+
+var drugsCensor = censor.bind(null,"drugs");
